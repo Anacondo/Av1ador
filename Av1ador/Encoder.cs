5899f93312f2b678abbb1d85439a5211e28731ae
Anacondo <5350602+Anacondo@users.noreply.github.com>
2025-11-26 12:28:23 +0100

    Fixed issue with video metadata

--- Av1ador/Encoder.cs
+++ Av1ador/Encoder.cs
@@ -520,7 +520,7 @@ namespace Av1ador
         public string Build_vstr(bool predict = false)
         {
             string str = " -init_hw_device vulkan:" + Vkn_Device;
-            str += " -hide_banner -copyts -start_at_zero -display_rotation 0 -vsync -1 -y !seek! -threads " + Threads.ToString() + " -i \"!file!\" !start! !duration! -c:v:0 " + Cv;
+            str += " -hide_banner -copyts -start_at_zero -display_rotation 0 -vsync -1 -threads " + Threads.ToString() + " -y !seek! -i \"!file!\" !start! !duration! -c:v:0 " + Cv;
             List<string> vf = new List<string>(Vf);
             bool always_2p = Cv == "libvpx-vp9" && Regex.Match(Params, "auto-alt-ref [1-6]").Success;
 
@@ -563,8 +563,9 @@ namespace Av1ador
                 if (Gs_level > 0)
                     str += ":film-grain=" + Gs_level;
 
-                // for levels of parallelism
+                /* for levels of parallelism
                 str += ":lp=" + Threads;
+                */
 
                 // for color and HDR handling
                 if (!Hdr)
