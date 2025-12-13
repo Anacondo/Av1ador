29d95dcb21e6a1392ce265621624e61be93a75d9
Anacondo <5350602+Anacondo@users.noreply.github.com>
2025-12-13 21:36:16 +0100

    Better defautls for SVT-AV1-HDR

--- Av1ador/Encoder.cs
+++ Av1ador/Encoder.cs
@@ -195,11 +195,11 @@ namespace Av1ador
                 Cv = "libsvtav1";
                 Max_crf = 63;
                 Crf = 18;
-                Bit_depth = new string[] { "10", "8" };
+                Bit_depth = new string[] { "10" };
                 Job = j[1];
                 Presets = new string[] { "-1 (slowest)", "0", "1", "2", "3", "*4", "5", "6", "7", "8", "9", "10", "11", "12 (fastest)" };
                 speed_str = "-preset ";
-                Params = "-svtav1-params tune=3:keyint=240:enable-qm=1:qm-min=8:qm-max=15:aq-mode=2:enable-dlf=2:enable-overlays=1:enable-restoration=0:enable-tf=2:tf-strength=1:enable-cdef=1:sharpness=1:enable-variance-boost=1:qp-scale-compress-strength=3:noise-norm-strength=3:psy-rd=1.0:spy-rd=1:luminance-qp-bias=10:hbd-mds=1:complex-hvs=1:sharp-tx=1:variance-boost-strength=3:variance-octile=4";
+                Params = "-svtav1-params tune=4:keyint=240:enable-qm=1:qm-min=8:qm-max=15:aq-mode=2:enable-dlf=2:enable-overlays=1:enable-restoration=0:enable-tf=0:tf-strength=0:enable-cdef=0:sharpness=1:enable-variance-boost=1:variance-boost-curve=3:qp-scale-compress-strength=3:noise-norm-strength=3:ac-bias=4.0:tx-bias=1:luminance-qp-bias=5:hbd-mds=1:complex-hvs=1:sharp-tx=1";
                 Color = ":color-primaries=1:transfer-characteristics=1:matrix-coefficients=1";
                 Gs = 50;
                 Rate = 0.85;
